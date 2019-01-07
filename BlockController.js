99676447ac9246ef3d3397b9dab85b30f28c79dd
Cristian Flores <ceflores11@gmail.com>
2019-01-07 14:34:23 -0800

    Added user validation request functionality
    AddRequestValidation function handles user validation requests made by submitting a POST with a given wallet address.

--- BlockController.js
+++ BlockController.js
@@ -4,11 +4,17 @@ const BlockChain = require('./BlockChain.js');
 const Boom = require('boom')
 const bitcoin = require('bitcoinjs-lib');
 const bitcoinMessage = require('bitcoinjs-message');
+const TimeoutRequestsWindowTime = 5*60*1000;
 
+// test variables
 let address = '142BDCeSGbXjWKaAnYXbMpZ6sbrSAo3DpZ'
 let signature = 'IJtpSFiOJrw/xYeucFxsHvIRFJ85YSGP8S1AEZxM4/obS3xr9iz7H0ffD7aM2vugrRaCi/zxaPtkflNzt5ykbc0='
 let message = '142BDCeSGbXjWKaAnYXbMpZ6sbrSAo3DpZ:1532330740:starRegistry'
+
 let myBlockChain = new BlockChain.Blockchain();
+let mempoolValid = [];
+let mempool = [];
+let timeoutRequests = [];
 
 // console.log(bitcoinMessage.verify(message, address, signature));
 
@@ -46,8 +52,85 @@ class BlockController {
 		  
 		this.getBlockByIndex();
         this.postNewBlock();
+		this.AddRequestValidation();
     }
 
+    /**
+     * Implement a POST Endpoint to requestValidation for walletAddress submitted for a window of 5 minutes.
+	 * url: "/requestValidation"
+     */
+    AddRequestValidation() {
+        this.server.route({
+            method: 'POST',
+            path: '/requestValidation',
+            handler: (request, h) => {
+				
+				const self = this;
+				const payload = request.payload;
+				const walletAddress = payload.address;
+				
+				let timeStamped = request.info.received.toString();
+				timeStamped = timeStamped.slice(0,-3);
+				
+				let message = walletAddress + ":" + timeStamped + ":starRegistry";
+				
+				// Calculate time left
+				let timeElapse = (new Date().getTime().toString().slice(0,-3)) - timeStamped;
+			    let timeLeft = (TimeoutRequestsWindowTime/1000) - timeElapse;
+				
+				// store request Object
+				let requestObject = JSON.parse(`{"walletAddress":"${walletAddress}", 
+												"requestTimeStamp":"${timeStamped}", 
+												"message":"${message}", 
+												"validationWindow":"${timeLeft}"}`);
+				
+				if(!payload) {
+					throw Boom.badData('Your data is bad and you should feel bad - No payload content!');
+				}
+				
+				if(!walletAddress) {
+					throw Boom.badData('Your data is bad and you should feel bad - No address content!');
+				}
+				
+				// check if walletAddress already in mempool - if yes, return original request object
+				if(walletAddress in mempool) {
+					console.log("Wallet address already in mempool. Returning previous request");
+					
+					return(mempool[walletAddress]);
+				}
+				
+				// save walletAddress in mempool
+				mempool[walletAddress]=requestObject;
+				timeoutRequests[walletAddress]=setTimeout(function(){ self.removeValidationRequest(walletAddress) }, TimeoutRequestsWindowTime );
+				
+				console.log("TimeoutRequestsWindowTime: " + TimeoutRequestsWindowTime);
+				console.log("current date: " + (new Date().getTime().toString()));
+				console.log("timeElapse: " + timeElapse);
+				
+				//let script = bitcoin.address.toOutputScript(payload.address, bitcoin.networks.bitcoin);
+				//let hash = bitcoin.crypto.sha256(script);
+				//let reversedHash = Buffer.from(hash.reverse());
+
+				//console.log(payload.address, ' maps to ', reversedHash.toString('hex'));
+				//console.log(reversedHash);
+				
+				return(requestObject);
+            }
+        });
+    }
+	
+	async removeValidationRequest(walletAddress) {
+		try {
+			let currentTime = (new Date().getTime().toString().slice(0,-3));
+			
+			console.log(`Removing ${walletAddress} from mempool at ${currentTime} :(`);
+			return await timeoutRequests.splice(walletAddress, 1);
+		}
+		catch (err) {
+			throw Boom.badData('Could not remove requested index from timeoutRequests: ' + err);
+		}
+	}
+	
     /**
      * Implement a GET Endpoint to retrieve a block by index, url: "/block/{index}"
      */
